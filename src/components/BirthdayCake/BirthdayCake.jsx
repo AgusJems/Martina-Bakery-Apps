7ab17844f3c27204648f8adc8dfc7fabf73df128
AgusJems <59831192+AgusJems@users.noreply.github.com>
2025-05-27 22:02:33 +0700

    update

--- src/components/BirthdayCake/BirthdayCake.jsx
+++ src/components/BirthdayCake/BirthdayCake.jsx
@@ -7,38 +7,44 @@ const BirthDayCakeData = [
     {
         id:1,
         img: Img1,
-        name: "Birhtday Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        name: "Birhtday Cake 18 cm",
+        price: "Rp. 85.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
     {
         id:2,
-        img: Img2,
-        name: "Brownies Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        img: Img1,
+        name: "Birhtday Cake 20 cm",
+        price: "Rp. 110.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
     {
         id:3,
-        img: Img3,
-        name: "Bolu Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        img: Img1,
+        name: "Birhtday Cake 22 cm",
+        price: "Rp. 150.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
     {
         id:4,
-        img: Img3,
-        name: "Bolu Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        img: Img2,
+        name: "Blackforest Cake 18 cm",
+        price: "Rp. 90.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
     {
         id:5,
-        img: Img3,
-        name: "Bolu Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        img: Img2,
+        name: "Blackforest Cake 20 cm",
+        price: "Rp. 120.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
     {
         id:6,
-        img: Img3,
-        name: "Bolu Cake",
-        description: "Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
+        img: Img2,
+        name: "Blackforest Cake 22 cm",
+        price: "Rp. 170.000",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit amet ipsum dolor sit"
     },
 ]
 
@@ -57,7 +63,7 @@ const BirthdayCake = () => {
                 </div>
                 <div> 
                     <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-4 md:gap-5 place-items-center">
-                        {BirthDayCakeData.map(({id, img, name, description}) => {
+                        {BirthDayCakeData.map(({id, img, name, price, description}) => {
                             return <div
                                 data-aos="zoom-out"
                                 data-aos-duration="300" 
@@ -67,11 +73,12 @@ const BirthdayCake = () => {
                                     </div> */}
                                     <div className="p-4 text-center">
                                         <img src={img} alt="" className="max-w-[120px] mx-auto group-hover:scale-105 group-hover:rotate-6 duration-300 mb-3"/>
-                                        <h1 className="text-md font-[Merienda] font-bold">{name}</h1>
+                                        <h1 className="text-md font-[Merienda] font-bold mb-3">{name}</h1>
+                                        <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2 mb-3">{price}</p>
                                         <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2 mb-3">{description}</p>
                                         <button className="bg-gradient-to-r from-gdtwo to-gdone text-white px-6 py-3 rounded-full hover:scale-105 duration-200" onClick={() => {
                                                 // const imageUrl = `https://martina-bakery-apps.vercel.app/website/${img}`;
-                                                const message = `Hello, saya ingin memesan:\n\nNama: ${name}\nDeskripsi: ${description}`;
+                                                const message = `Hello, saya ingin memesan:\n\nNama: ${name}\nHarga: ${price}`;
                                                 const waUrl = `https://wa.me/6285876064192?text=${encodeURIComponent(message)}`;
                                                 window.open(waUrl, '_blank');
                                             }}>Order</button>
